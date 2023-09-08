7d3900d643d2c1b7ae42ed1b11d083ee63ffc1c7
CodeInMyBones <135045416+CodeInMyBones@users.noreply.github.com>
2023-09-09 00:18:26 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,12 @@
-file herr
+#include <stdio.h>
+
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+
+int main(void)
+{
+	puts("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
